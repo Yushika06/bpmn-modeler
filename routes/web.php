b05c5bda20a12c96cd4c64c8b70d6dbdc653c5e1
Yushika06 <yuukionime06@gmail.com>
2024-07-30 21:43:24 +0700

    auth: login-register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect']);
+Route::get('/auth/google/callback',  [AuthController::class, 'callback']);
 
 require __DIR__.'/auth.php';
