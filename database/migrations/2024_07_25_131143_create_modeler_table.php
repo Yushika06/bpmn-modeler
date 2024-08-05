0d7c70206158eda059e37a6498c8a40f5c5d43b8
Yushika06 <yuukionime06@gmail.com>
2024-08-05 20:15:05 +0700

    pages: profile

--- database/migrations/2024_07_25_131143_create_modeler_table.php
+++ database/migrations/2024_07_25_131143_create_modeler_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateModelersTable extends Migration
+class CreateModelerTable extends Migration
 {
     public function up()
     {
-        Schema::create('modelers', function (Blueprint $table) {
+        Schema::create('modeler', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->longText('bpmn');
